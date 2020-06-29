05a58b4f273e1e83feb21bed1162b9520310b713
particle <particle1331@gmail.com>
2020-06-29 17:28:06 +0800

    version 0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='recommender-model-builder',
-    version='0.0.1',
+    version='0.1.0',
     description='',
     py_modules=["recommenderutils"],
     package_dir={"": "src"},
